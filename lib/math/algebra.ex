53ef3f40b7905dad4ba364985cee31f7de3bf2d1
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-09-30 15:14:27 +0300

    test first_minor

--- lib/math/algebra.ex
+++ lib/math/algebra.ex
@@ -2,8 +2,40 @@ defmodule Math.Algebra do
   @moduledoc false
 
   @doc ~S"""
-  A value that can be computed from the elements of a square matrix.
-  Geometrically, it can be viewed as the scaling factor of the linear transformation described by the matrix.
+  The `first_minor` of a `matrix` obtained by removing just the `i`-row and the `j`-column from the `matrix`.
+  It is required for calculating **cofactors**, which in turn are useful for computing both the **determinant**
+  and **inverse** of square matrices.
+
+  ## Examples
+
+      iex> Math.Algebra.minor([[ 1.0, 4.0, 7.0],
+      ...>                     [ 3.0, 0.0, 5.0],
+      ...>                     [-1.0, 9.0,11.0]],
+      ...>                     2,
+      ...>                     3)
+      13.0
+
+      iex> Math.Algebra.minor([[6.0, 1.0, 1.0],
+      ...>                     [4.0,-2.0, 5.0],
+      ...>                     [2.0, 8.0, 7.0]],
+      ...>                     1,
+      ...>                     1)
+      -54.0
+
+      iex> Math.Algebra.determinant([[ 5.0,-7.0, 2.0, 2.0],
+      ...>                           [ 0.0, 3.0, 0.0,-4.0],
+      ...>                           [-5.0,-8.0, 0.0, 3.0],
+      ...>                           [ 0.0, 5.0, 0.0,-6.0]],
+      ...>                           1,
+      ...>                           3)
+      5.0
+
+  """
+  def first_minor(matrix, i, j), do: nil
+
+  @doc ~S"""
+  A value that can be computed from the elements of a square `matrix`.
+  Geometrically, it can be viewed as the _scaling factor_ of the _linear transformation_ described by the `matrix`.
 
   ## Examples
 
