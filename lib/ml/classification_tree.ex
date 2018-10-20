e31acefea2bb5d6e51ea329109f3816d967316ae
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-10-21 00:49:00 +0300

    refactoring: break down

--- lib/ml/classification_tree.ex
+++ lib/ml/classification_tree.ex
@@ -34,53 +34,61 @@ defmodule Ml.ClassificationTree do
     entropy(dataset, class_attr) - entropy(dataset, class_attr, attribute)
   end
 
-  def decision_tree([row | _] = dataset, class_attr, non_selected_attrs) do
+  defp exausted_attributes(dataset, class_attr) do
+    %Utils.Pair{first: fst, second: _} = dataset
+                                         |> Enum.group_by(fn row -> row[class_attr] end)
+                                         |> Enum.map(fn {k, v} -> %Utils.Pair{first: k, second: length(v)} end)
+                                         |> Enum.sort_by(&(&1.second), &>=/2)
+                                         |> Enum.at(0)
+    %Utils.TreeNode{
+      content: %{
+        class: fst
+      },
+      children: []
+    }
+  end
+
+  defp same_class([row | _], class_attr) do
+    %Utils.TreeNode{
+      content: %{
+        class: row[class_attr]
+      },
+      children: []
+    }
+  end
+
+  defp break_down(dataset, class_attr, non_selected_attrs) do
+    [selected_attr | rest_attrs] = Enum.sort_by(
+      non_selected_attrs,
+      &(information_gain(dataset, class_attr, &1)),
+      &>=/2
+    )
+    grouped_by_attr = Enum.group_by(dataset, fn row -> row[selected_attr] end)
+    %Utils.TreeNode{
+      content: %{
+        attribute: selected_attr
+      },
+      children: Enum.map(
+        grouped_by_attr,
+        fn {k, v} -> %Utils.TreeNode{
+                       content: %{
+                         value: k
+                       },
+                       children: decision_tree(v, class_attr, rest_attrs)
+                     }
+        end
+      )
+    }
+  end
+
+  def decision_tree(dataset, class_attr, non_selected_attrs) do
     grouped_by_class = Enum.group_by(dataset, fn row -> row[class_attr] end)
-    [
-      cond do
-        Enum.empty?(non_selected_attrs) ->
-          %Utils.Pair{first: fst, second: _} = dataset
-                                               |> Enum.group_by(fn row -> row[class_attr] end)
-                                               |> Enum.map(fn {k, v} -> %Utils.Pair{first: k, second: length(v)} end)
-                                               |> Enum.sort_by(&(&1.second), &>=/2)
-                                               |> Enum.at(0)
-          %Utils.TreeNode{
-            content: %{
-              class: fst
-            },
-            children: []
-          }
-        Enum.count(grouped_by_class) == 1 ->
-          %Utils.TreeNode{
-            content: %{
-              class: row[class_attr]
-            },
-            children: []
-          }
-        true ->
-          [selected_attr | rest_attrs] = Enum.sort_by(
-            non_selected_attrs,
-            &(information_gain(dataset, class_attr, &1)),
-            &>=/2
-          )
-          grouped_by_attr = Enum.group_by(dataset, fn row -> row[selected_attr] end)
-          %Utils.TreeNode{
-            content: %{
-              attribute: selected_attr
-            },
-            children: Enum.map(
-              grouped_by_attr,
-              fn {k, v} -> %Utils.TreeNode{
-                             content: %{
-                               value: k
-                             },
-                             children: decision_tree(v, class_attr, rest_attrs)
-                           }
-              end
-            )
-          }
-      end
-    ]
+    tree = cond do
+      Enum.empty?(non_selected_attrs) -> exausted_attributes(dataset, class_attr)
+      Enum.count(grouped_by_class) == 1 -> same_class(dataset, class_attr)
+      true -> break_down(dataset, class_attr, non_selected_attrs)
+    end
+    [tree]
   end
 
 end
