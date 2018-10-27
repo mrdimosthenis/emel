77a76fac2969a4288771656273590d523511cd90
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-10-27 15:05:37 +0300

    bring back utils tests

---
+++ test/help/utils_test.exs
@@ -0,0 +1,29 @@
+defmodule Help.UtilsTest do
+  @moduledoc false
+  use ExUnit.Case
+  doctest Help.Utils
+  alias Help.Utils
+
+  @leaf_a %Utils.TreeNode{
+    content: %{
+      class: "y"
+    }
+  }
+  @leaf_b %Utils.TreeNode{
+    content: %{
+      class: "n"
+    }
+  }
+  @node %Utils.TreeNode{
+    children: [@leaf_a, @leaf_b],
+    content: %{
+      value: "f"
+    }
+  }
+
+  test "pretty tree" do
+    assert Utils.pretty_tree(@leaf_a) == %{class: "y"}
+    assert Utils.pretty_tree(@node) == [%{value: "f"}, [%{class: "y"}, %{class: "n"}]]
+  end
+
+end
