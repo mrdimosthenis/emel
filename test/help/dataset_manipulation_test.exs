0af836388be60b9a0d834c1e9be6920985a3080e
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-10-27 16:31:50 +0300

    import instead of alias in DatasetManipulation test

--- test/help/dataset_manipulation_test.exs
+++ test/help/dataset_manipulation_test.exs
@@ -2,25 +2,25 @@ defmodule Help.DatasetManipulationTest do
   @moduledoc false
   use ExUnit.Case
   doctest Help.DatasetManipulation
-  alias Help.DatasetManipulation
+  import Help.DatasetManipulation
 
   test "category" do
-    f1 = DatasetManipulation.categorizer([:negative, 0, :non_negative])
+    f1 = categorizer([:negative, 0, :non_negative])
     assert f1.(-3) == :negative
     assert f1.(1) == :non_negative
 
-    f2 = DatasetManipulation.categorizer(["very small", 3, "small", 7, "moderate", 12, "large"])
+    f2 = categorizer(["very small", 3, "small", 7, "moderate", 12, "large"])
     assert f2.(1) == "very small"
     assert f2.(6) == "small"
     assert f2.(10) == "moderate"
     assert f2.(100) == "large"
 
     assert_raise RuntimeError, "Categories are not separated by valid thresholds", fn ->
-      _ = DatasetManipulation.categorizer([:negative, 0, :non_negative, -1, :whatever])
+      _ = categorizer([:negative, 0, :non_negative, -1, :whatever])
     end
 
     assert_raise RuntimeError, "Categories are not separated by valid thresholds", fn ->
-      _ = DatasetManipulation.categorizer([:negative, :whatever])
+      _ = categorizer([:negative, :whatever])
     end
   end
 
