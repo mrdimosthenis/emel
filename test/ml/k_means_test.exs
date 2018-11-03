2e400d4183de3bd2453bb48e94a224f4839cf6c4
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-03 13:50:38 +0200

    normalize iris

--- test/ml/k_means_test.exs
+++ test/ml/k_means_test.exs
@@ -4,22 +4,34 @@ defmodule KMeansTest do
   import Ml.KMeans
   alias Help.DatasetManipulation
 
+  def parse(flower) do
+    flower
+    |> Enum.map(
+         fn {k, v} -> case Enum.member?(["petal_length", "petal_width", "sepal_length", "sepal_width"], k) do
+                        true -> {k, DatasetManipulation.parse(v)}
+                        false -> {k, v}
+                      end
+         end
+       )
+    |> Map.new()
+  end
+
   def flower_to_point(%{"petal_length" => pl, "petal_width" => pw, "sepal_length" => sl, "sepal_width" => sw}) do
-    {parsed_petal_length, ""} = Float.parse(pl)
-    {parsed_petal_width, ""} = Float.parse(pw)
-    {parsed_sepal_length, ""} = Float.parse(sl)
-    {parsed_sepal_width, ""} = Float.parse(sw)
-    [parsed_sepal_length, parsed_sepal_width, parsed_petal_length, parsed_petal_width]
+    [pl, pw, sl, sw]
   end
 
   test "k-means on iris dataset" do
     {training_set, test_set} = "resources/datasets/iris.csv"
                                |> DatasetManipulation.load_dataset()
-                               |> DatasetManipulation.training_and_test_sets(0.65)
+                               |> DatasetManipulation.training_and_test_sets(0.80)
     f = training_set
+        |> Enum.map(&parse/1)
+        |> DatasetManipulation.normalize(["petal_length", "petal_width", "sepal_length", "sepal_width"])
         |> Enum.map(&flower_to_point/1)
         |> classifier(3)
     predicted_classes = test_set
+                        |> Enum.map(&parse/1)
+                        |> DatasetManipulation.normalize(["petal_length", "petal_width", "sepal_length", "sepal_width"])
                         |> Enum.map(&flower_to_point/1)
                         |> Enum.map(fn row -> f.(row) end)
     actual_classes = Enum.map(
@@ -33,7 +45,7 @@ defmodule KMeansTest do
       end
     )
     score = DatasetManipulation.similarity(predicted_classes, actual_classes)
-    assert score == 0.9622641509433962
+    assert score == 0.9666666666666667
   end
 
 end
