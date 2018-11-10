7999b251b57b8957fea1633de1cc433697be8513
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-10 11:34:59 +0200

    comment out package name

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Emel.MixProject do
   defp package() do
     [
       # This option is only needed when you don't want to use the OTP application name
-      name: "emel",
+      # name: "emel",
       # These are the default files included in the package
       # files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
       #           license* CHANGELOG* changelog* src),
