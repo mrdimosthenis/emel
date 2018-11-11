ed3d85d4a26988958adc44c1297644f40da57f8c
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-11 13:43:55 +0200

    renaming

--- lib/ml/perceptron.ex
+++ lib/ml/perceptron.ex
@@ -4,16 +4,16 @@ defmodule Ml.Perceptron do
   alias Math.Geometry
   alias Help.Utils
 
-  def weights_for_example(ws, x, y, a) when (length(ws) == length(x)) do
+  def weights_updated(ws, x, y, a) when is_number(y) and (length(ws) == length(x)) do
     y_hat = Geometry.dot_product(ws, x)
     for i <- Utils.indices(ws) do
       Enum.at(ws, i) + a * (y - y_hat) * Enum.at(x, i)
     end
   end
 
-  def weights_for_group(ws, xs, ys, a) do
+  def weights_updated(ws, [x | _] = xs, ys, a) when (length(xs) == length(ys)) and (length(ws) == length(x)) do
     Enum.zip(xs, ys)
-    |> Enum.reduce(ws, fn ({x, y}, acc) -> weights_for_example(acc, x, y, a) end)
+    |> Enum.reduce(ws, fn ({x, y}, acc) -> weights_updated(acc, x, y, a) end)
   end
 
 end
--- test/ml/perceptron_test.exs
+++ test/ml/perceptron_test.exs
@@ -3,25 +3,23 @@ defmodule Ml.PerceptronTest do
   doctest Ml.Perceptron
   import Ml.Perceptron
 
-  test "weights_for_example" do
-    assert weights_for_example([0.0, 0.0, 0.0], [1, 0, 0], 0, 0.5) == [0.0, 0.0, 0.0]
-    assert weights_for_example([0.0, 0.0, 0.0], [1, 0, 1], 0, 0.5) == [0.0, 0.0, 0.0]
-    assert weights_for_example([0.0, 0.0, 0.0], [1, 1, 0], 0, 0.5) == [0.0, 0.0, 0.0]
-    assert weights_for_example([0.0, 0.0, 0.0], [1, 1, 1], 1, 0.5) == [0.5, 0.5, 0.5]
+  test "weights_updated" do
+    assert weights_updated([0.0, 0.0, 0.0], [1, 0, 0], 0, 0.5) == [0.0, 0.0, 0.0]
+    assert weights_updated([0.0, 0.0, 0.0], [1, 0, 1], 0, 0.5) == [0.0, 0.0, 0.0]
+    assert weights_updated([0.0, 0.0, 0.0], [1, 1, 0], 0, 0.5) == [0.0, 0.0, 0.0]
+    assert weights_updated([0.0, 0.0, 0.0], [1, 1, 1], 1, 0.5) == [0.5, 0.5, 0.5]
 
-    assert weights_for_example([0.5, 0.5, 0.5], [1, 0, 0], 0, 0.5) == [0.25, 0.5, 0.5]
-    assert weights_for_example([0.25, 0.5, 0.5], [1, 0, 1], 0, 0.5) == [-0.125, 0.5, 0.125]
-    assert weights_for_example([-0.125, 0.5, 0.125], [1, 1, 0], 0, 0.5) == [-0.3125, 0.3125, 0.125]
-    assert weights_for_example([-0.3125, 0.3125, 0.125], [1, 1, 1], 1, 0.5) == [0.125, 0.75, 0.5625]
+    assert weights_updated([0.5, 0.5, 0.5], [1, 0, 0], 0, 0.5) == [0.25, 0.5, 0.5]
+    assert weights_updated([0.25, 0.5, 0.5], [1, 0, 1], 0, 0.5) == [-0.125, 0.5, 0.125]
+    assert weights_updated([-0.125, 0.5, 0.125], [1, 1, 0], 0, 0.5) == [-0.3125, 0.3125, 0.125]
+    assert weights_updated([-0.3125, 0.3125, 0.125], [1, 1, 1], 1, 0.5) == [0.125, 0.75, 0.5625]
 
-    assert weights_for_example([0.125, 0.75, 0.5625], [1, 0, 0], 0, 0.5) == [0.0625, 0.75, 0.5625]
-    assert weights_for_example([0.0625, 0.75, 0.5625], [1, 0, 1], 0, 0.5) == [-0.25, 0.75, 0.25]
-    assert weights_for_example([-0.25, 0.75, 0.25], [1, 1, 0], 0, 0.5) == [-0.5, 0.5, 0.25]
-    assert weights_for_example([-0.5, 0.5, 0.25], [1, 1, 1], 1, 0.5) == [-0.125, 0.875, 0.625]
-  end
+    assert weights_updated([0.125, 0.75, 0.5625], [1, 0, 0], 0, 0.5) == [0.0625, 0.75, 0.5625]
+    assert weights_updated([0.0625, 0.75, 0.5625], [1, 0, 1], 0, 0.5) == [-0.25, 0.75, 0.25]
+    assert weights_updated([-0.25, 0.75, 0.25], [1, 1, 0], 0, 0.5) == [-0.5, 0.5, 0.25]
+    assert weights_updated([-0.5, 0.5, 0.25], [1, 1, 1], 1, 0.5) == [-0.125, 0.875, 0.625]
 
-  test "weights_for_group" do
-    assert weights_for_group(
+    assert weights_updated(
              [0.0, 0.0, 0.0],
              [
                [1, 0, 0],
@@ -33,7 +31,7 @@ defmodule Ml.PerceptronTest do
              0.5
            ) == [0.5, 0.5, 0.5]
 
-    assert weights_for_group(
+    assert weights_updated(
              [0.5, 0.5, 0.5],
              [
                [1, 0, 0],
@@ -45,7 +43,7 @@ defmodule Ml.PerceptronTest do
              0.5
            ) == [0.125, 0.75, 0.5625]
 
-    assert weights_for_group(
+    assert weights_updated(
              [0.125, 0.75, 0.5625],
              [
                [1, 0, 0],
