ec5dc6593a70a6a4e0a5d7d74c898bf50246f872
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-17 17:16:59 +0200

    need to take into acount the input values when weights are updated

--- lib/ml/artificial_neuron.ex
+++ lib/ml/artificial_neuron.ex
@@ -8,13 +8,22 @@ defmodule Ml.ArtificialNeuron do
 
   alias Math.Calculus
   alias Math.Geometry
+  alias Help.Utils
+  alias Math.Statistics
 
   defmodule Neuron do
     @moduledoc false
-    @enforce_keys [:ws, :f, :f_deriv]
-    defstruct [:ws, :f, :f_deriv]
+    @enforce_keys [:ws, :fun]
+    defstruct [:ws, :fun]
 
-    def new(ws, f \\ &Calculus.logistic_function/1, f_deriv \\ &Calculus.logistic_derivative/1) do
+    def new(
+          ws,
+          {f, der_f, inv_f} \\ {
+            &Calculus.logistic_function/1,
+            &Calculus.logistic_derivative/1,
+            &Calculus.logistic_inverse/1
+          }
+        ) do
       weights = if is_number(ws) do
         for _ <- 1..ws do
           :rand.uniform()
@@ -23,14 +32,75 @@ defmodule Ml.ArtificialNeuron do
         ws
       end
 
-      %Neuron{ws: weights, f: f, f_deriv: f_deriv}
+      %Neuron{ws: weights, fun: {f, der_f, inv_f}}
     end
   end
 
-  def neuron_output(xs, %Neuron{ws: ws, f: f}) do
-    xs
+  def neuron_output(%Neuron{ws: ws, fun: {f, _, _}}, x) do
+    x
     |> Geometry.dot_product(ws)
     |> f.()
+  end
+
+  def update_neuron_weights(%Neuron{ws: ws, fun: {f, der_f, inv_f}}, target_val, output_val, a) do
+    error = target_val - output_val
+    deriv = output_val
+            |> inv_f.()
+            |> der_f.()
+    delta = deriv * error
+    weights = Enum.map(ws, fn w -> (1 + a * delta) * w end)
+    %Neuron{ws: weights, fun: {f, der_f, inv_f}}
+  end
+
+  defp iterate(neuron, _, _, _, _, 0), do: neuron
+  defp iterate(%Neuron{ws: ws, fun: {f, der_f, inv_f}} = neuron, xs, ys, a, err_thres, max_iter) do
+    IO.puts(inspect(ws))
+    IO.puts("")
+    new_neuron = Enum.reduce(
+      Enum.zip(xs, ys),
+      neuron,
+      fn ({x, y}, acc) ->
+        output = neuron_output(neuron, x)
+        update_neuron_weights(acc, y, output, a)
+      end
+    )
+    mean_abs_err = xs
+                   |> Enum.map(fn x -> neuron_output(new_neuron, x) end)
+                   |> Statistics.mean_absolute_error(ys)
+    case mean_abs_err < err_thres do
+      true -> new_neuron
+      false -> iterate(new_neuron, xs, ys, a, err_thres, max_iter - 1)
+    end
+  end
+
+  def classifier(
+        dataset,
+        continuous_attributes,
+        boolean_class,
+        {f, der_f, inv_f} \\ {
+          &Calculus.logistic_function/1,
+          &Calculus.logistic_derivative/1,
+          &Calculus.logistic_inverse/1
+        },
+        learning_rate \\ 0.0001,
+        err_thres \\ 0.1,
+        max_iter \\ 10000
+      ) do
+    [x | _] = xs = Enum.map(dataset, fn row -> [1 | Utils.map_vals(row, continuous_attributes)] end)
+    ys = Enum.map(
+      dataset,
+      fn row -> case row[boolean_class] do
+                  true -> 1
+                  false -> 0
+                end
+      end
+    )
+    init_neuron = Neuron.new(length(x) + 1)
+    neuron = iterate(init_neuron, xs, ys, learning_rate, err_thres, max_iter)
+    fn item ->
+      item_vals = [1 | Utils.map_vals(item, continuous_attributes)]
+      neuron_output(neuron, item_vals) >= 0.5
+    end
   end
 
 end
--- test/ml/artificial_neuron_test.exs
+++ test/ml/artificial_neuron_test.exs
@@ -3,11 +3,69 @@ defmodule ArtificialNeuronTest do
   doctest Ml.ArtificialNeuron
   import Ml.ArtificialNeuron
   alias Ml.ArtificialNeuron.Neuron
+  alias Help.Model
+  alias Help.ModelTest
+  alias Help.Io
+  alias Math.Statistics
 
   test "neuron_output" do
     neuron = Neuron.new([0.3, 0.5, 0.9])
-    xs = [0.73, 0.79, 0.69]
-    assert neuron_output(xs, neuron) == 0.7746924929149283
+    x = [0.73, 0.79, 0.69]
+    assert neuron_output(neuron, x) == 0.7746924929149283
   end
-  
+
+  test "update_neuron_weights" do
+    neuron = Neuron.new([0.3, 0.5, 0.9])
+
+    assert update_neuron_weights(neuron, 0, 0.5, 1).ws == [0.2625, 0.4375, 0.7875]
+    assert update_neuron_weights(neuron, 1, 0.5, 1).ws == [0.33749999999999997, 0.5625, 1.0125]
+    assert update_neuron_weights(neuron, 0.5, 0.5, 1).ws == [0.3, 0.5, 0.9]
+
+    assert update_neuron_weights(neuron, 0, 0.5, 0.3).ws == [0.28875, 0.48125, 0.8662500000000001]
+    assert update_neuron_weights(neuron, 1, 0.5, 0.3).ws == [0.31125, 0.51875, 0.9337500000000001]
+    assert update_neuron_weights(neuron, 0.5, 0.5, 0.3).ws == [0.3, 0.5, 0.9]
+  end
+
+  test "artificial neuron on titanic dataset" do
+    {training_set, test_set} = "resources/datasets/titanic.csv"
+                               |> Io.load_dataset(
+                                    ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]
+                                  )
+                               |> Enum.filter(
+                                    fn row ->
+                                      row
+                                      |> Map.values()
+                                      |> Enum.all?(&(&1 != ""))
+                                    end
+                                  )
+                               |> ModelTest.continuous_passenger_attributes()
+                               |> Model.normalize(["Pclass", "Age", "SibSp", "Parch", "Fare"])
+                               |> Enum.map(
+                                    fn %{"Survived" => survived, "Sex" => sex} = row ->
+                                      %{
+                                        row |
+                                        "Survived" => case survived do
+                                          "0" -> false
+                                          "1" -> true
+                                        end,
+                                        "Sex" => case sex do
+                                          "male" -> 1
+                                          "female" -> 0
+                                        end
+                                      }
+                                    end
+                                  )
+                               |> Model.training_and_test_sets(0.90)
+    f = classifier(training_set, ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"], "Survived",
+                                           {
+                                           &Calculus.logistic_function/1,
+                                           &Calculus.logistic_derivative/1,
+                                           &Calculus.logistic_inverse/1
+                                            }, 0.001, 0.01, 10)
+    predicted_classes = Enum.map(test_set, fn row -> f.(row) end)
+    actual_classes = Enum.map(test_set, fn %{"Survived" => sv} -> sv end)
+    score = Statistics.similarity(predicted_classes, actual_classes)
+    assert score == 0.8194444444444444
+  end
+
 end
