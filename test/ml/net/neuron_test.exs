64f2c0b8a652bd3071895317d8715500932081dc
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-20 01:32:34 +0200

    test fire_ys

--- test/ml/net/neuron_test.exs
+++ test/ml/net/neuron_test.exs
@@ -1,6 +1,24 @@
 defmodule Ml.Net.NeuronTest do
-  @moduledoc false
+  use ExUnit.Case
+  doctest Ml.ArtificialNeuron
+  import Ml.Net.Neuron
 
-  alias Ml.Net.Neuron
+  test "fire_ys" do
+    {:ok, pid} = start_link(2, 0.5, [self()])
+    refute_receive {:x, _, _}
+    send pid, {:x, 0.05, :x1}
+    refute_receive {:x, _, _}
+    send pid, {:x, 0.15, :x2}
+    assert_receive {:x, 0.5437845658942952, _}
+
+    {:ok, pid} = start_link(3, 0.5, [self(), self()])
+    refute_receive {:x, _, _}
+    send pid, {:x, 0.95, :x1}
+    refute_receive {:x, _, _}
+    send pid, {:x, 0.85, :x2}
+    refute_receive {:x, _, _}
+    send pid, {:x, 0.95, :x3}
+    assert_receive {:x, 0.8750889379213852, _}
+  end
 
 end
