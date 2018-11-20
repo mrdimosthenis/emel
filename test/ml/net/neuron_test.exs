4a2f21d7634d959fe274ab4bde577c0f56bdb511
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-20 03:43:26 +0200

    need to clear state of neuron

--- test/ml/net/neuron_test.exs
+++ test/ml/net/neuron_test.exs
@@ -13,17 +13,23 @@ defmodule Ml.Net.NeuronTest do
     send pid, {:x, 0.0, Utils.useless_process}
     assert_receive {:x, 0.5, _}
 
-#    refute_receive {:y, _, _}
-#    send pid, {:y, 0.0, self()}
-#    assert_receive {:y, 0.0, _}
+    refute_receive {:y, _, _}
+    send pid, {:y, 0.0, self()}
+    assert_receive {:y, 0.21152913565577777, _}
   end
 
   test "single input - double output" do
-    {:ok, pid} = start_link(1, 0.5, [self(), self()])
+    temp_process = Utils.useless_process
+
+    {:ok, pid} = start_link(1, 0.5, [self(), temp_process])
 
     refute_receive {:x, _, _}
-    send pid, {:x, 0.05, Utils.useless_process}
+    send pid, {:x, 0.05, temp_process}
     assert_receive {:x, 0.5071455518077935, _}
+
+#    refute_receive {:y, _, _}
+#    send pid, {:y, 0.7, self()}
+#    assert_receive {:y, 0.21152913565577777, _}
 
   end
 
@@ -31,7 +37,7 @@ defmodule Ml.Net.NeuronTest do
     {:ok, pid} = start_link(3, 0.5, [self()])
 
     refute_receive {:x, _, _}
-    send pid, {:x, 0.95, Utils.useless_process}
+    send pid, {:x, 0.95, self()}
     refute_receive {:x, _, _}
     send pid, {:x, 0.85, Utils.useless_process}
     refute_receive {:x, _, _}
