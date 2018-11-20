09e998b4e042ecf41f4c3668d1f0b99d8b696167
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-20 02:39:48 +0200

    separate test by "story"

--- test/ml/net/neuron_test.exs
+++ test/ml/net/neuron_test.exs
@@ -3,27 +3,31 @@ defmodule Ml.Net.NeuronTest do
   doctest Ml.ArtificialNeuron
   import Ml.Net.Neuron
 
-  test "fire_ys" do
+  test "double input - single output" do
     {:ok, pid} = start_link(2, 0.5, [self()])
     refute_receive {:x, _, _}
     send pid, {:x, 0.0, :x1}
     refute_receive {:x, _, _}
     send pid, {:x, 0.0, :x2}
     assert_receive {:x, 0.5, _}
+  end
 
-    {:ok, pid} = start_link(1, 0.5, [self()])
+  test "single input - double output" do
+    {:ok, pid} = start_link(1, 0.5, [self(), self()])
     refute_receive {:x, _, _}
     send pid, {:x, 0.05, :x1}
-    assert_receive {:x, 0.5059232769328919, _}
+    assert_receive {:x, 0.5071455518077935, _}
+  end
 
-    {:ok, pid} = start_link(3, 0.5, [self(), self()])
+  test "triple input - single output" do
+    {:ok, pid} = start_link(3, 0.5, [self()])
     refute_receive {:x, _, _}
     send pid, {:x, 0.95, :x1}
     refute_receive {:x, _, _}
     send pid, {:x, 0.85, :x2}
     refute_receive {:x, _, _}
     send pid, {:x, 0.95, :x3}
-    assert_receive {:x, 0.837011287834731, _}
+    assert_receive {:x, 0.8695904405699183, _}
   end
 
 end
