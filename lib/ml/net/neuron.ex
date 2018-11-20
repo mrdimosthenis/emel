825d0c5cabc3a02c66f56be41e70b32a8b513daf
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-20 13:15:55 +0200

    state as struct

--- lib/ml/net/neuron.ex
+++ lib/ml/net/neuron.ex
@@ -5,6 +5,11 @@ defmodule Ml.Net.Neuron do
   alias Math.Geometry
   alias Math.Calculus
 
+  defmodule State do
+    @moduledoc false
+    defstruct [:ws, :x_by_pid, :d_by_pid, :a]
+  end
+
   def start_link(num_of_inputs, learning_rate, ys_ids) do
     a = learning_rate
     ws = Utils.rand_float(num_of_inputs)
@@ -12,10 +17,11 @@ defmodule Ml.Net.Neuron do
     nils = Utils.duplicate(nil, length(ys_ids))
     d_by_pid = Enum.zip(ys_ids, nils)
                |> Map.new()
-    Task.start_link(fn -> loop({ws, x_by_pid, d_by_pid, a}) end)
+    state = %State{ws: ws, x_by_pid: x_by_pid, d_by_pid: d_by_pid, a: a}
+    Task.start_link(fn -> loop(state) end)
   end
 
-  defp forward({ws, x_by_pid, d_by_pid, _}) do
+  defp forward(%State{ws: ws, x_by_pid: x_by_pid, d_by_pid: d_by_pid}) do
     y = x_by_pid
         |> Enum.sort_by(fn {id, _} -> id end)
         |> Enum.map(fn {_, x} -> x end)
@@ -24,7 +30,7 @@ defmodule Ml.Net.Neuron do
     Enum.each(d_by_pid, fn {pid, _} -> send pid, {:x, y, self()} end)
   end
 
-  defp backward({ws, x_by_pid, d_by_pid, a}) do
+  defp backward(%State{ws: ws, x_by_pid: x_by_pid, d_by_pid: d_by_pid, a: a}) do
     common_factor = d_by_pid
                     |> Map.values()
                     |> Enum.sum()
@@ -42,10 +48,10 @@ defmodule Ml.Net.Neuron do
     new_d_by_pid = d_by_pid
                    |> Enum.map(fn {pid, _} -> {pid, nil} end)
                    |> Map.new()
-    loop({new_ws, new_x_by_pid, new_d_by_pid, a})
+    loop(%State{ws: new_ws, x_by_pid: new_x_by_pid, d_by_pid: new_d_by_pid, a: a})
   end
 
-  defp maybe_act({ws, x_by_pid, d_by_pid, _} = state) do
+  defp maybe_act(%State{ws: ws, x_by_pid: x_by_pid, d_by_pid: d_by_pid} = state) do
     cond do
       Enum.all?(d_by_pid, fn {_, d} -> d != nil end) -> backward(state)
       Enum.count(x_by_pid) == length(ws) -> forward(state)
@@ -53,15 +59,15 @@ defmodule Ml.Net.Neuron do
     end
   end
 
-  defp loop({ws, x_by_pid, d_by_pid, a} = state) do
+  defp loop(%State{ws: ws, x_by_pid: x_by_pid, d_by_pid: d_by_pid, a: a} = state) do
     maybe_act(state)
     receive do
       {:x, x, caller} ->
         new_x_by_pid = Map.put(x_by_pid, caller, x)
-        loop({ws, new_x_by_pid, d_by_pid, a})
+        loop(%State{ws: ws, x_by_pid: new_x_by_pid, d_by_pid: d_by_pid, a: a})
       {:y, d, caller} ->
         new_d_by_pid = %{d_by_pid | caller => d}
-        loop({ws, x_by_pid, new_d_by_pid, a})
+        loop(%State{ws: ws, x_by_pid: x_by_pid, d_by_pid: new_d_by_pid, a: a})
     end
   end
 
