f8d78d201e555dafaf4ee6a56683e2461182e976
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-21 02:13:11 +0200

    genserver's first attempt

---
+++ lib/ml/net/fitted_neuron.ex
@@ -0,0 +1,91 @@
+defmodule Ml.Net.FittedNeuron do
+  @moduledoc false
+
+  alias Help.Utils
+  alias Math.Geometry
+  alias Math.Calculus
+
+  use GenServer
+
+  defmodule State do
+    @moduledoc false
+    defstruct [:ws, :xpids_with_vals, :ypids_with_ds, :a, :status]
+  end
+
+  # Client
+
+  def start_link(default) do
+    GenServer.start_link(__MODULE__, default)
+  end
+
+  def fire(pid, xpid, xval) do
+    GenServer.cast(pid, {:fire, xpid, xval})
+  end
+
+  def back_propagate(pid, ypid, dval) do
+    GenServer.cast(pid, {:back_propagate, ypid, dval})
+  end
+
+  # Server (callbacks)
+
+  @impl true
+  def init([n, a, ypids]) do
+    ws = Utils.rand_float(n)
+    xpids_with_vals = []
+    ypids_with_ds = Enum.zip(ypids, Stream.cycle([nil]))
+    state = %State{ws: ws, xpids_with_vals: xpids_with_vals, ypids_with_ds: ypids_with_ds, a: a, status: :wait_xpids}
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_cast(
+        {:fire, xpid, xval},
+        %State{ws: ws, xpids_with_vals: xpids_with_vals, ypids_with_ds: ypids_with_ds, a: a, status: status} = state
+      ) do
+    new_xpids_with_vals = Utils.put_into_keylist(xpids_with_vals, xpid, xval)
+    new_status = if (Enum.count(new_xpids_with_vals) == length(ws) && status == :wait_xpids) ||
+                      (Enum.all?(new_xpids_with_vals, fn {_, x} -> x != nil end) && status == :wait_xvals) do
+      y = new_xpids_with_vals
+          |> Enum.map(fn {_, x} -> x end)
+          |> Geometry.dot_product(ws)
+          |> Calculus.logistic_function()
+      Enum.each(ypids_with_ds, fn {ypid, _} -> fire(ypid, self(), y) end)
+      :wait_dvals
+    else
+      status
+    end
+    {:noreply, %{state | xpids_with_vals: new_xpids_with_vals, status: new_status}}
+  end
+  def handle_cast(
+        {:back_propagate, ypid, dval},
+        %State{ws: ws, xpids_with_vals: xpids_with_vals, ypids_with_ds: ypids_with_ds, a: a, status: status} = state
+      ) do
+    new_ypids_with_ds = Utils.put_into_keylist(ypids_with_ds, ypid, dval)
+    if Enum.all?(new_ypids_with_ds, fn {_, d} -> d != nil end) && status == :wait_dvals do
+      common_factor = new_ypids_with_ds
+                      |> Enum.map(fn {_, d} -> d end)
+                      |> Enum.sum()
+                      |> Calculus.logistic_derivative()
+      xpids_with_vals
+      |> Enum.map(fn {xpid, _} -> xpid end)
+      |> Enum.zip(ws)
+      |> Enum.each(fn {xpid, w} -> back_propagate(xpid, self(), common_factor * w) end)
+      new_ws = xpids_with_vals
+               |> Enum.map(fn {_, x} -> x end)
+               |> Enum.zip(ws)
+               |> Enum.map(fn {x, w} -> w - a * common_factor * x end)
+      new_xpids_with_vals = Enum.map(xpids_with_vals, fn {xpid, _} -> {xpid, nil} end)
+      cleared_ypids_with_ds = Enum.map(new_ypids_with_ds, fn {ypid, _} -> {ypid, nil} end)
+      %State{
+        ws: new_ws,
+        xpids_with_vals: new_xpids_with_vals,
+        ypids_with_ds: cleared_ypids_with_ds,
+        a: a,
+        status: :wait_xvals
+      }
+    else
+      %{state | ypids_with_ds: new_ypids_with_ds}
+    end
+  end
+
+end
