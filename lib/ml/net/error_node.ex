4ff6693e1b573fd5978fdf009324c10501d81aaf
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-25 20:31:30 +0200

    imperative error node

--- lib/ml/net/error_node.ex
+++ lib/ml/net/error_node.ex
@@ -1,17 +1,29 @@
 defmodule Ml.Net.ErrorNode do
   @moduledoc false
 
+  import List, only: [replace_at: 3]
+
+  alias Help.Utils
+
   use GenServer
 
   defmodule State do
     @moduledoc false
-    defstruct [:n, :vals_and_hats_by_yhatpid]
+    defstruct [:x_pids, :xs, :y_pids, :ys]
   end
 
   # Client
 
   def start_link(default) do
     GenServer.start_link(__MODULE__, default)
+  end
+
+  def set_x_pids(pid, x_pids) do
+    GenServer.call(pid, {:set_x_pids, x_pids})
+  end
+
+  def set_y_pids(pid, y_pids) do
+    GenServer.call(pid, {:set_y_pids, y_pids})
   end
 
   def stop(pid) do
@@ -21,52 +33,82 @@ defmodule Ml.Net.ErrorNode do
   # Server (callbacks)
 
   @impl true
+
   def init(n) do
-    state = %State{n: n, vals_and_hats_by_yhatpid: %{}}
+    xs = Utils.duplicate(nil, n)
+    ys = Utils.duplicate(nil, n)
+    state = %State{xs: xs, ys: ys}
     {:ok, state}
   end
 
   @impl true
 
-  def handle_info({:fire_y, yhatpid, yval}, %State{vals_and_hats_by_yhatpid: vals_and_hats_by_yhatpid} = state) do
-    old_vals_and_hats = vals_and_hats_by_yhatpid[yhatpid]
-    new_vals_and_hats = case old_vals_and_hats do
-      nil -> %{y: yval}
-      %{} = map -> Map.put(map, :y, yval)
-    end
-    new_vals_and_hats_by_yhatpid = Map.put(vals_and_hats_by_yhatpid, yhatpid, new_vals_and_hats)
-    new_state = %{state | vals_and_hats_by_yhatpid: new_vals_and_hats_by_yhatpid}
-    maybe_back_propagate(new_state)
+  def handle_call({:set_x_pids, x_pids}, _from, state) do
+    new_state = %{state | x_pids: x_pids}
+    {:reply, :ok, new_state}
   end
 
-  def handle_info({:fire, xpid, xval}, %State{vals_and_hats_by_yhatpid: vals_and_hats_by_yhatpid} = state) do
-    old_vals_and_hats = vals_and_hats_by_yhatpid[xpid]
-    new_vals_and_hats = case old_vals_and_hats do
-      nil -> %{yhat: xval}
-      %{} = map -> Map.put(map, :yhat, xval)
-    end
-    new_vals_and_hats_by_yhatpid = Map.put(vals_and_hats_by_yhatpid, xpid, new_vals_and_hats)
-    new_state = %{state | vals_and_hats_by_yhatpid: new_vals_and_hats_by_yhatpid}
-    maybe_back_propagate(new_state)
+  def handle_call({:set_y_pids, y_pids}, _from, state) do
+    new_state = %{state | y_pids: y_pids}
+    {:reply, :ok, new_state}
+  end
+
+  @impl true
+
+  def handle_info(
+        {:fire, x_pid, x_val},
+        %State{
+          x_pids: x_pids,
+          xs: xs
+        } = state
+      ) do
+    index = Enum.find_index(x_pids, fn pid -> pid == x_pid end)
+    temp_xs = replace_at(xs, index, x_val)
+    temp_state = %{state | xs: temp_xs}
+    {new_xs, new_ys} = maybe_back_propagate(temp_state)
+    new_state = %{state | xs: new_xs, ys: new_ys}
+    {:noreply, new_state}
+  end
+
+  def handle_info(
+        {:fire_y, y_pid, y_val},
+        %State{
+          y_pids: y_pids,
+          ys: ys
+        } = state
+      ) do
+    index = Enum.find_index(y_pids, fn pid -> pid == y_pid end)
+    temp_ys = replace_at(ys, index, y_val)
+    temp_state = %{state | ys: temp_ys}
+    {new_xs, new_ys} = maybe_back_propagate(temp_state)
+    new_state = %{state | xs: new_xs, ys: new_ys}
+    {:noreply, new_state}
   end
 
   # helper functions
 
-  defp maybe_back_propagate(%State{n: n, vals_and_hats_by_yhatpid: vals_and_hats_by_yhatpid} = state) do
-    new_state = if Enum.count(vals_and_hats_by_yhatpid) == n &&
-                     Enum.all?(
-                       vals_and_hats_by_yhatpid,
-                       fn {_, vals_and_hats} -> vals_and_hats[:y] != nil && vals_and_hats[:yhat] != nil end
-                     ) do
-      for {yhatpid, %{y: y, yhat: yhat}} <- vals_and_hats_by_yhatpid do
-        delta = (-2) * (y - yhat)
-        send(yhatpid, {:back_propagate, self(), delta})
-      end
-      %{state | vals_and_hats_by_yhatpid: %{}}
-    else
-      state
+  defp maybe_back_propagate(
+         %State{
+           x_pids: x_pids,
+           xs: xs,
+           y_pids: y_pids,
+           ys: ys
+         }
+       ) do
+    case Enum.all?(xs, fn x -> x != nil end) && Enum.all?(ys, fn y -> y != nil end) do
+      true -> Enum.zip([x_pids, xs, y_pids, ys])
+              |> Enum.each(
+                   fn {x_pid, x, y_pid, y} ->
+                     delta = (-2) * (y - x)
+                     send(x_pid, {:back_propagate, self(), delta})
+                     send(y_pid, {:back_propagate, self(), delta})
+                   end
+                 )
+              new_xs = Enum.map(xs, fn _ -> nil end)
+              new_ys = Enum.map(ys, fn _ -> nil end)
+              {new_xs, new_ys}
+      _ -> {xs, ys}
     end
-    {:noreply, new_state}
   end
 
 end
--- test/ml/net/error_node_test.exs
+++ test/ml/net/error_node_test.exs
@@ -4,67 +4,69 @@ defmodule Ml.Net.ErrorNodeTest do
   alias Ml.Net.ErrorNode
   alias Help.Utils
 
-  test "single output" do
-    {:ok, pid} = GenServer.start_link(ErrorNode, 1)
+  test "single output - neuron side" do
+    input_node = Utils.useless_process()
+    neuron = self()
+
+    {:ok, error_node} = GenServer.start_link(ErrorNode, 1)
+    ErrorNode.set_x_pids(error_node, [neuron])
+    ErrorNode.set_y_pids(error_node, [input_node])
 
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, self(), 0.85})
+    send(error_node, {:fire_y, input_node, 0.85})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, self(), 0.85})
+    send(error_node, {:fire, neuron, 0.85})
     assert_receive {:back_propagate, _, 0.0}
 
+    send(error_node, {:fire_y, input_node, 0.6})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, self(), 0.6})
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, self(), 0.85})
+    send(error_node, {:fire, neuron, 0.85})
     assert_receive {:back_propagate, _, 0.5}
 
+    send(error_node, {:fire, neuron, 0.6})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, self(), 0.85})
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, self(), 0.6})
+    send(error_node, {:fire_y, input_node, 0.85})
     assert_receive {:back_propagate, _, -0.5}
   end
 
-  test "triple output" do
-    temp_process_a = Utils.useless_process()
-    temp_process_b = Utils.useless_process()
+  test "triple output - input side" do
+    input_node_a = Utils.useless_process()
+    input_node_b = self()
+    input_node_c = Utils.useless_process()
+    neuron_a = Utils.useless_process()
+    neuron_b = Utils.useless_process()
+    neuron_c = Utils.useless_process()
 
-    {:ok, pid} = GenServer.start_link(ErrorNode, 3)
-
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, temp_process_a, 0.85})
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, temp_process_a, 0.85})
-
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, self(), 0.6})
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, self(), 0.85})
-    refute_receive {:back_propagate, _, _}
+    {:ok, error_node} = GenServer.start_link(ErrorNode, 3)
+    ErrorNode.set_x_pids(error_node, [neuron_a, neuron_b, neuron_c])
+    ErrorNode.set_y_pids(error_node, [input_node_a, input_node_b, input_node_c])
 
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, temp_process_b, 0.6})
+    send(error_node, {:fire_y, input_node_a, 0.85})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, temp_process_b, 0.6})
+    send(error_node, {:fire, neuron_a, 0.85})
+    refute_receive {:back_propagate, _, _}
+    send(error_node, {:fire_y, input_node_b, 0.6})
+    refute_receive {:back_propagate, _, _}
+    send(error_node, {:fire, neuron_b, 0.85})
+    refute_receive {:back_propagate, _, _}
+    send(error_node, {:fire_y, input_node_c, 0.6})
+    refute_receive {:back_propagate, _, _}
+    send(error_node, {:fire, neuron_c, 0.6})
 
     assert_receive {:back_propagate, _, 0.5}
 
+    send(error_node, {:fire_y, input_node_c, 0.85})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, temp_process_a, 0.85})
+    send(error_node, {:fire, neuron_a, 0.6})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, temp_process_a, 0.85})
-
+    send(error_node, {:fire_y, input_node_b, 1.0})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, self(), 1.0})
+    send(error_node, {:fire, neuron_b, 0.0})
     refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, self(), 0.0})
+    send(error_node, {:fire, neuron_c, 0.85})
     refute_receive {:back_propagate, _, _}
-
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire_y, temp_process_b, 0.6})
-    refute_receive {:back_propagate, _, _}
-    send(pid, {:fire, temp_process_b, 0.6})
+    send(error_node, {:fire_y, input_node_a, 0.6})
 
     assert_receive {:back_propagate, _, -2.0}
   end
