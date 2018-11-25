03214df800fe78bfb634d2eb474e59e75839ee3e
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-25 10:28:24 +0200

    imperative neuron

--- lib/ml/net/neuron.ex
+++ lib/ml/net/neuron.ex
@@ -1,6 +1,8 @@
 defmodule Ml.Net.Neuron do
   @moduledoc false
 
+  import List, only: [replace_at: 3]
+
   alias Help.Utils
   alias Math.Geometry
   alias Math.Calculus
@@ -9,13 +11,33 @@ defmodule Ml.Net.Neuron do
 
   defmodule State do
     @moduledoc false
-    defstruct [:ws, :xpids_with_vals, :ypids]
+    defstruct [:ws, :x_pids, :xs, :y_pids, :ys, :fit, :a]
   end
 
   # Client
 
   def start_link(default) do
     GenServer.start_link(__MODULE__, default)
+  end
+
+  def set_ws(pid, ws) do
+    GenServer.call(pid, {:set_ws, ws})
+  end
+
+  def get_ws(pid) do
+    GenServer.call(pid, :get_ws)
+  end
+
+  def set_x_pids(pid, x_pids) do
+    GenServer.call(pid, {:set_x_pids, x_pids})
+  end
+
+  def set_y_pids(pid, y_pids) do
+    GenServer.call(pid, {:set_y_pids, y_pids})
+  end
+
+  def set_fit(pid, fit) do
+    GenServer.call(pid, {:set_fit, fit})
   end
 
   def stop(pid) do
@@ -25,27 +47,105 @@ defmodule Ml.Net.Neuron do
   # Server (callbacks)
 
   @impl true
-  def init([ws, ypids]) do
-    state = %State{ws: ws, xpids_with_vals: [], ypids: ypids}
+
+  def init([xn, yn, a]) do
+    ws = Utils.rand_float(xn)
+    xs = Utils.duplicate(nil, xn)
+    ys = Utils.duplicate(nil, yn)
+    state = %State{ws: ws, xs: xs, ys: ys, fit: true, a: a}
+    {:ok, state}
+  end
+
+  def init([xn, yn]) do
+    ws = Utils.rand_float(xn)
+    xs = Utils.duplicate(nil, xn)
+    ys = Utils.duplicate(nil, yn)
+    state = %State{ws: ws, xs: xs, ys: ys, fit: false}
     {:ok, state}
   end
 
   @impl true
 
-  def handle_info({:fire, xpid, xval}, %State{ws: ws, xpids_with_vals: xpids_with_vals, ypids: ypids} = state) do
-    new_xpids_with_vals = Utils.put_into_keylist(xpids_with_vals, xpid, xval)
-    final_xpids_with_vals = if Enum.count(new_xpids_with_vals) == length(ws) &&
-                                 Enum.all?(new_xpids_with_vals, fn {_, x} -> x != nil end) do
-      y = new_xpids_with_vals
-          |> Enum.map(fn {_, x} -> x end)
-          |> Geometry.dot_product(ws)
-          |> Calculus.logistic_function()
-      Enum.each(ypids, fn ypid -> send(ypid, {:fire, self(), y}) end)
-      Enum.map(new_xpids_with_vals, fn {xpids, _} -> {xpids, nil} end)
-    else
-      new_xpids_with_vals
+  def handle_call({:set_ws, ws}, _from, state) do
+    new_state = %{state | ws: ws}
+    {:reply, :ok, new_state}
+  end
+
+  def handle_call(:get_ws, _from, %State{ws: ws} = state) do
+    {:reply, ws, state}
+  end
+
+  def handle_call({:set_x_pids, x_pids}, _from, state) do
+    new_state = %{state | x_pids: x_pids}
+    {:reply, :ok, new_state}
+  end
+
+  def handle_call({:set_y_pids, y_pids}, _from, state) do
+    new_state = %{state | y_pids: y_pids}
+    {:reply, :ok, new_state}
+  end
+
+  def handle_call({:set_fit, fit}, _from, state) do
+    new_state = %{state | fit: fit}
+    {:reply, :ok, new_state}
+  end
+
+  @impl true
+
+  def handle_info(
+        {:fire, x_pid, x_val},
+        %State{
+          ws: ws,
+          x_pids: x_pids,
+          xs: xs,
+          y_pids: y_pids,
+          fit: fit
+        } = state
+      ) do
+    index = Enum.find_index(x_pids, fn pid -> pid == x_pid end)
+    temp_xs = replace_at(xs, index, x_val)
+    new_xs = case Enum.all?(temp_xs, fn x -> x != nil end) do
+      true -> y_val = Geometry.dot_product(ws, temp_xs)
+                      |> Calculus.logistic_function()
+              Enum.each(y_pids, fn pid -> send(pid, {:fire, self(), y_val}) end)
+              case fit do
+                true -> temp_xs
+                false -> Enum.map(temp_xs, fn _ -> nil end)
+              end
+      _ -> temp_xs
     end
-    new_state = %{state | xpids_with_vals: final_xpids_with_vals}
+    new_state = %{state | xs: new_xs}
+    {:noreply, new_state}
+  end
+
+  def handle_info(
+        {:back_propagate, y_pid, y_val},
+        %State{
+          ws: ws,
+          x_pids: x_pids,
+          xs: xs,
+          ys: ys,
+          y_pids: y_pids,
+          a: a
+        } = state
+      ) do
+    index = Enum.find_index(y_pids, fn pid -> pid == y_pid end)
+    temp_ys = List.replace_at(ys, index, y_val)
+    {new_ws, new_xs, new_ys} = case Enum.all?(temp_ys, fn y -> y != nil end) do
+      true -> theta = Geometry.dot_product(ws, xs)
+                      |> Calculus.logistic_function()
+              delta = Enum.sum(temp_ys)
+              common_factor = delta * Calculus.logistic_derivative(theta)
+              Enum.zip(x_pids, ws)
+              |> Enum.each(fn {pid, w} -> send(pid, {:back_propagate, self(), common_factor * w}) end)
+              ws_ = Enum.zip(xs, ws)
+                    |> Enum.map(fn {x, w} -> w - a * common_factor * x end)
+              xs_ = Enum.map(xs, fn _ -> nil end)
+              ys_ = Enum.map(temp_ys, fn _ -> nil end)
+              {ws_, xs_, ys_}
+      _ -> {ws, xs, temp_ys}
+    end
+    new_state = %{state | ws: new_ws, xs: new_xs, ys: new_ys}
     {:noreply, new_state}
   end
 
--- test/ml/net/neuron_test.exs
+++ test/ml/net/neuron_test.exs
@@ -2,123 +2,52 @@ defmodule Ml.Net.NeuronTest do
   use ExUnit.Case, async: true
   doctest Ml.Net.Neuron
   alias Ml.Net.Neuron
-  alias Ml.Net.InputNode
-  alias Ml.Net.ErrorNode
-  alias Ml.Net.FitNeuron
-  alias Ml.Net.BiasNode
+  alias Help.Utils
 
-  test "neuron on XOR function" do
-    {:ok, error_node} = GenServer.start_link(ErrorNode, 1)
+  test "double input - single output" do
+    x_pid_a = Utils.useless_process()
+    x_pid_b = Utils.useless_process()
+    y_pid = self()
 
-    {:ok, fit_terminal_neuron} = GenServer.start_link(FitNeuron, [[0.5, 0.5, 0.5], 0.0001, [error_node]])
+    {:ok, neuron} = GenServer.start_link(Neuron, [2, 1, 0.5])
+    Neuron.set_ws(neuron, [0.5, 0.5])
+    Neuron.set_x_pids(neuron, [x_pid_a, x_pid_b])
+    Neuron.set_y_pids(neuron, [y_pid])
 
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_a, 0.0})
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_b, 0.0})
+    assert_receive {:fire, _, 0.5}
 
-    {:ok, fit_neuron_a} = GenServer.start_link(FitNeuron, [[0.5, 0.5, 0.5], 0.0001, [fit_terminal_neuron]])
-    {:ok, fit_neuron_b} = GenServer.start_link(FitNeuron, [[0.5, 0.5, 0.5], 0.0001, [fit_terminal_neuron]])
-    {:ok, _} = GenServer.start_link(BiasNode, [fit_terminal_neuron])
+    assert Neuron.get_ws(neuron) == [0.5, 0.5]
 
-    {:ok, input_node_a} = GenServer.start_link(InputNode, [fit_neuron_a, fit_neuron_b])
-    {:ok, input_node_b} = GenServer.start_link(InputNode, [fit_neuron_a, fit_neuron_b])
-    {:ok, _} = GenServer.start_link(BiasNode, [fit_neuron_a, fit_neuron_b])
+    send(neuron, {:back_propagate, y_pid, 0.0})
+    assert Neuron.get_ws(neuron) == [0.5, 0.5]
 
-    send(input_node_a, {:fire, self(), 0})
-    refute_receive {:back_propagate, _, _}
-    send(input_node_b, {:fire, self(), 0})
-    refute_receive {:back_propagate, _, _}
-    send(error_node, {:fire_y, fit_terminal_neuron, 0})
-    assert_receive {:back_propagate, _, 0.03284572958030721}
-    assert_receive {:back_propagate, _, 0.03284572958030721}
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_a, 1.0})
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_b, 1.0})
+    assert_receive {:fire, _, 0.7310585786300049}
 
-    send(input_node_a, {:fire, self(), 1})
-    refute_receive {:back_propagate, _, _}
-    send(input_node_b, {:fire, self(), 0})
-    refute_receive {:back_propagate, _, _}
-    send(error_node, {:fire_y, fit_terminal_neuron, 1})
-    assert_receive {:back_propagate, _, -0.007772698519283291}
-    assert_receive {:back_propagate, _, -0.007772698519283291}
+    send(neuron, {:back_propagate, y_pid, 0.7})
+    assert Neuron.get_ws(neuron) == [0.4232233475965673, 0.4232233475965673]
 
-    send(input_node_a, {:fire, self(), 0})
-    refute_receive {:back_propagate, _, _}
-    send(input_node_b, {:fire, self(), 1})
-    refute_receive {:back_propagate, _, _}
-    send(error_node, {:fire_y, fit_terminal_neuron, 1})
-    assert_receive {:back_propagate, _, -0.007772615928359264}
-    assert_receive {:back_propagate, _, -0.007772628011199327}
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_a, 1.0})
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_b, 1.0})
+    assert_receive {:fire, _, 0.6998212248866079}
 
-    send(input_node_a, {:fire, self(), 1})
-    refute_receive {:back_propagate, _, _}
-    send(input_node_b, {:fire, self(), 1})
-    refute_receive {:back_propagate, _, _}
-    send(error_node, {:fire_y, fit_terminal_neuron, 0})
-    assert_receive {:back_propagate, _, 0.019600216869179003}
-    assert_receive {:back_propagate, _, 0.01960021686950276}
+    send(neuron, {:back_propagate, y_pid, -0.9})
+    assert Neuron.get_ws(neuron) == [0.5230001398259432, 0.5230001398259432]
 
-    wait_back_propagate = fn ->
-      Enum.each(
-        1..2,
-        fn _ ->
-          receive do
-            {:back_propagate, _, _} -> :ok
-          end
-        end
-      )
-    end
-
-    for _ <- 1 .. 10000 do
-      send(input_node_a, {:fire, self(), 0})
-      send(input_node_b, {:fire, self(), 0})
-      send(error_node, {:fire_y, fit_terminal_neuron, 0})
-      wait_back_propagate.()
-
-      send(input_node_a, {:fire, self(), 1})
-      send(input_node_b, {:fire, self(), 0})
-      send(error_node, {:fire_y, fit_terminal_neuron, 1})
-      wait_back_propagate.()
-
-      send(input_node_a, {:fire, self(), 0})
-      send(input_node_b, {:fire, self(), 1})
-      send(error_node, {:fire_y, fit_terminal_neuron, 1})
-      wait_back_propagate.()
-
-      send(input_node_a, {:fire, self(), 1})
-      send(input_node_b, {:fire, self(), 1})
-      send(error_node, {:fire_y, fit_terminal_neuron, 0})
-      wait_back_propagate.()
-    end
-
-    terminal_weights = FitNeuron.get_weights(fit_terminal_neuron)
-    neuron_a_weights = FitNeuron.get_weights(fit_neuron_a)
-    neuron_b_weights = FitNeuron.get_weights(fit_neuron_b)
-
-    assert terminal_weights == [0.26628779983831447, 0.26628779983831447, 0.16952519813522637]
-    assert neuron_a_weights == [0.49272667025648476, 0.492726651660333, 0.4758581575187801]
-    assert neuron_b_weights == [0.49272667025648476, 0.492726651660333, 0.4758581575187801]
-
-#    {:ok, terminal_neuron} = GenServer.start_link(Neuron, [terminal_weights, [self()]])
-#
-#    {:ok, neuron_a} = GenServer.start_link(Neuron, [neuron_a_weights, [terminal_neuron]])
-#    {:ok, neuron_b} = GenServer.start_link(Neuron, [neuron_b_weights, [terminal_neuron]])
-#
-#    {:ok, input_node_a} = GenServer.start_link(InputNode, [neuron_a, neuron_b])
-#    {:ok, input_node_b} = GenServer.start_link(InputNode, [neuron_a, neuron_b])
-#
-#    {:ok, terminal_bias} = GenServer.start_link(BiasNode, [terminal_neuron])
-#    {:ok, hidden_bias} = GenServer.start_link(BiasNode, [neuron_a, neuron_b])
-#
-#    fire_biases = fn ->
-#      BiasNode.fire(terminal_bias)
-#      BiasNode.fire(hidden_bias)
-#    end
-#
-#    send(input_node_a, {:fire, self(), 0})
-#    send(input_node_b, {:fire, self(), 0})
-#    assert_receive {:fire, _, 0.48570317427986925}
-#
-#    fire_biases.()
-#    send(input_node_a, {:fire, self(), 1})
-#    send(input_node_b, {:fire, self(), 0})
-#    assert_receive {:fire, _, 0.6446960120359124}
-
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_a, 1.0})
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_b, 1.0})
+    assert_receive {:fire, _, 0.7400061037414002}
   end
 
 end
