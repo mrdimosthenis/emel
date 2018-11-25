d7cfb80207629eb9ead3f62b9b94a90cba90b576
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-11-25 10:58:59 +0200

    complete test for neuron

--- test/ml/net/neuron_test.exs
+++ test/ml/net/neuron_test.exs
@@ -63,7 +63,52 @@ defmodule Ml.Net.NeuronTest do
     refute_receive {:fire, _, _}
     send(neuron, {:fire, x_pid_b, 1.0})
     assert_receive {:fire, _, 0.7400061037414002}
+  end
 
+  test "single input - double output - x side" do
+    x_pid = self()
+    y_pid_a = Utils.useless_process()
+    y_pid_b = Utils.useless_process()
+
+    {:ok, neuron} = GenServer.start_link(Neuron, [1, 2, 0.8])
+    Neuron.set_ws(neuron, [0.5])
+    Neuron.set_x_pids(neuron, [x_pid])
+    Neuron.set_y_pids(neuron, [y_pid_a, y_pid_b])
+
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid, 0.4})
+    refute_receive {:fire, _, _}
+
+    send(neuron, {:back_propagate, y_pid_a, 0.7})
+    refute_receive {:back_propagate, _, _}
+    send(neuron, {:back_propagate, y_pid_b, 0.1})
+    assert_receive {:back_propagate, _, 0.09280718965530217}
+  end
+
+  test "double input - single output - y side - no fitting" do
+    x_pid_a = Utils.useless_process()
+    x_pid_b = Utils.useless_process()
+    y_pid = self()
+
+    {:ok, neuron} = GenServer.start_link(Neuron, [2, 1])
+    Neuron.set_ws(neuron, [0.65, 0.45])
+    Neuron.set_x_pids(neuron, [x_pid_a, x_pid_b])
+    Neuron.set_y_pids(neuron, [y_pid])
+
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_a, 0.7})
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_b, 0.8})
+    assert_receive {:fire, _, 0.6931739493259501}
+
+    assert Neuron.get_ws(neuron) == [0.65, 0.45]
+
+    send(neuron, {:fire, x_pid_a, 0.1})
+    refute_receive {:fire, _, _}
+    send(neuron, {:fire, x_pid_b, 0.2})
+    assert_receive {:fire, _, 0.538672605206508}
+
+    assert Neuron.get_ws(neuron) == [0.65, 0.45]
   end
 
 end
